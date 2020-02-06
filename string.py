cfb48cb14435971535f420ab3e3547dc9a758dcd
Collinsniper <collinsniper01@gmail.com>
2020-02-06 09:58:21 +0300

    Python String

---
+++ string.py
@@ -0,0 +1,19 @@
+'''
+mystring
+string indexing
+string splitting
+string built in method()
+'''
+a="Hello World!"
+print(a)
+print(a[0])
+print(a[5])
+print(a[-1]) #Use negative indexes to start the slice from the end of the string:
+print(a[:])
+print(a[2:5]) # Values counted from 2 to 5 but 5 is excluded
+print(a[1])
+print(a[:8])
+print(a[::]) # 
+print(a[-2])
+print(a[6:10]) # Values counted from 6 to 10 but 10 is excluded
+print(a[0:4])  # Values counted from 0 to 4 but 4 is excluded
